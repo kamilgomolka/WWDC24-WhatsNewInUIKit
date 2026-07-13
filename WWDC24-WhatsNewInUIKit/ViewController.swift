cd3645d29e07094f4ca06cf9c54c3f61edfad18c
Kamil Gomolka <kamilgomolka@gmail.com>
2026-07-13 16:33:16 +0200

    Initial Commit

---
+++ WWDC24-WhatsNewInUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WWDC24-WhatsNewInUIKit
+//
+//  Created by Kamil Gomolka on 13/07/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
